a490661a586bd5038a901598eb626b42decc9ed7
Kathir1999 <113663134+Kathir1999@users.noreply.github.com>
2025-08-15 18:08:32 +0530

    app.ts message

--- app.ts
+++ app.ts
@@ -16,7 +16,8 @@ app.use("/uploads", express.static("uploads")); // For profile photos
 app.use("/employees", employeeRoutes);
 
 // Start server 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
